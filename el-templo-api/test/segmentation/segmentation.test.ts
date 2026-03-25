089a540d8e2c4ba1021d5a76345d8ee3b8a30bb0
Franco Miceli <francomiceli.94@gmail.com>
2026-03-25 00:45:25 -0300

    fix: restore createStaffUser import in segmentation test

--- el-templo-api/test/segmentation/segmentation.test.ts
+++ el-templo-api/test/segmentation/segmentation.test.ts
@@ -14,6 +14,7 @@ import {
   createTestApp,
   registerUser,
   getAuthToken,
+  createStaffUser,
 } from "../helpers";
 import * as schema from "../../src/db/schema";
 import { SegmentationService } from "../../src/modules/segmentation/service";
